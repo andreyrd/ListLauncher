217e8472e8de220d63549340f36dcc621ace6296
Andrey Radchishin <andreyrd1@hotmail.com>
2014-09-02 17:30:25 -0700

    Fast scroll & hold menu.

--- src/com/amagital/listlauncher/LauncherActivity.java
+++ src/com/amagital/listlauncher/LauncherActivity.java
@@ -1,65 +1,20 @@
 package com.amagital.listlauncher;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
+import android.app.AlertDialog;
+import android.content.*;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class LauncherActivity extends Activity {
-	private Context context;
-
-	private static class AppInfo implements Comparable {
-		private Intent intent;
-		private String name;
-		private Drawable icon;
-
-		public Intent getIntent() {
-			return intent;
-		}
-
-		public void setIntent(Intent intent) {
-			this.intent = intent;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public Drawable getIcon() {
-			return icon;
-		}
-
-		public void setIcon(Drawable icon) {
-			this.icon = icon;
-		}
-
-		@Override
-		public int compareTo(Object another) {
-			if (another instanceof AppInfo) {
-				return this.name.toLowerCase().compareTo(((AppInfo) another).name.toLowerCase());
-			} else {
-				return 0;
-			}
-		}
-	}
-
 	private ArrayList<AppInfo> appInfoList;
 
 	private LauncherAdapter listAdapter;
@@ -68,16 +23,13 @@ public class LauncherActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-		// Set up a pointer to the context for anonymous classes
-		this.context = this;
-
         setContentView(R.layout.launcher);
 
 		// Initialize the array with 0 capacity (will ensureCapacity later)
 		appInfoList = new ArrayList<AppInfo>(0);
 
 		ListView listView = (ListView) findViewById(R.id.launcher_list);
-		listAdapter = new LauncherAdapter();
+		listAdapter = new LauncherAdapter(this, appInfoList);
 
 		listView.setAdapter(listAdapter);
 
@@ -86,6 +38,14 @@ public class LauncherActivity extends Activity {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 				startActivity(appInfoList.get(position).getIntent());
+			}
+		});
+
+		listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view, int i, long id) {
+				showDetailsDialog(appInfoList.get(i));
+				return true;
 			}
 		});
     }
@@ -129,6 +89,9 @@ public class LauncherActivity extends Activity {
 					String name = info.loadLabel(pm).toString();
 
 					if (intent != null && name != null) {
+						intent.setAction(Intent.ACTION_MAIN);
+						intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
 						AppInfo appInfo = new AppInfo();
 
 						appInfo.setName(name);
@@ -147,45 +110,11 @@ public class LauncherActivity extends Activity {
 
 			@Override
 			protected void onPostExecute(Void aVoid) {
-				listAdapter.notifyDataSetChanged();
+				listAdapter.notifyDataSetChanged(appInfoList);
 			}
 		};
 
 		task.execute();
-	}
-
-	private class LauncherAdapter extends BaseAdapter {
-		@Override
-		public int getCount() {
-			return appInfoList.size();
-		}
-
-		@Override
-		public View getView(int position, View view, ViewGroup parent) {
-			if (view == null) {
-				view = View.inflate(context, R.layout.item, null);
-			}
-
-			TextView nameView = (TextView) view.findViewById(R.id.item_name);
-			ImageView iconView = (ImageView) view.findViewById(R.id.item_icon);
-
-			AppInfo info = appInfoList.get(position);
-
-			nameView.setText(info.getName());
-			iconView.setImageDrawable(info.getIcon());
-
-			return view;
-		}
-
-		@Override
-		public Object getItem(int position) {
-			return null;
-		}
-
-		@Override
-		public long getItemId(int position) {
-			return 0;
-		}
 	}
 
 	private class ChangeReceiver extends BroadcastReceiver {
@@ -193,6 +122,36 @@ public class LauncherActivity extends Activity {
 		public void onReceive(Context context, Intent intent) {
 			loadAppInfo();
 		}
+	}
+
+	private void showDetailsDialog(final AppInfo info) {
+		AlertDialog dialog = new AlertDialog.Builder(this)
+				.setItems(R.array.details, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						Uri packageUri = Uri.fromParts("package", info.getIntent().getPackage(), null);
+
+						switch (which) {
+							case 0:
+								// Open
+								startActivity(info.getIntent());
+								break;
+							case 1:
+								// Uninstall
+								Intent deleteIntent = new Intent(Intent.ACTION_DELETE, packageUri);
+								startActivity(deleteIntent);
+								break;
+							case 2:
+								// App Info
+								Intent infoIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+										packageUri);
+								startActivity(infoIntent);
+								break;
+						}
+					}
+				}).create();
+
+		dialog.show();
 	}
 
 	@Override
